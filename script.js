de97bf204f70db9665cc123d31ea884504563f61
tuticala <mcandelacalabrese@gmail.com>
2025-07-14 16:23:10 -0300

    Update script2.js

--- script.js
+++ script.js
@@ -19,4 +19,40 @@ function renderizarMalla() {
   for (let periodo of periodos) {
     const div = document.createElement('div');
     div.className = 'periodo';
-    div
+    div.innerHTML = `<h3>Período ${periodo}</h3>`;
+
+    const materiasPeriodo = materias.filter(m => m.periodo === periodo);
+    for (let m of materiasPeriodo) {
+      const matDiv = document.createElement('div');
+      matDiv.className = `materia ${m.tipologia}`;
+      matDiv.innerText = m.nombre;
+
+      const aprobada = materiasAprobadas.has(m.id);
+      const correlativasAprobadas = m.correlativas.every(cid => materiasAprobadas.has(cid));
+
+      if (!aprobada && !correlativasAprobadas) {
+        matDiv.classList.add("bloqueada");
+        matDiv.setAttribute("data-correlativas", `Faltan: ${m.correlativas.filter(cid => !materiasAprobadas.has(cid)).join(', ')}`);
+      }
+
+      if (aprobada) {
+        matDiv.classList.add("aprobada");
+      }
+
+      matDiv.addEventListener("click", () => {
+        if (aprobada) {
+          materiasAprobadas.delete(m.id);
+        } else if (correlativasAprobadas || m.correlativas.length === 0) {
+          materiasAprobadas.add(m.id);
+        }
+        renderizarMalla(); // Redibujar todo
+      });
+
+      div.appendChild(matDiv);
+    }
+
+    container.appendChild(div);
+  }
+}
+
+cargarMalla();
